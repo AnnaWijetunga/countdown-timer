96e5f6d2ea71102357a2a2f9cb768abdef1ed90e
AnnaWijetunga <annawijetunga@gmail.com>
2020-07-31 16:28:54 -0400

    Display the end time

--- scripts.js
+++ scripts.js
@@ -32,3 +32,11 @@ function timer(seconds) {
     timerDisplay.textContent = display;
   }
 
+  function displayEndTime(timestamp) {
+    const end = new Date(timestamp);
+    const hour = end.getHours();
+    const adjustedHour = hour > 12 ? hour - 12 : hour;
+    const minutes = end.getMinutes();
+    endTime.textContent = `Be Back At ${adjustedHour}:${minutes < 10 ? '0' : ''}${minutes}`;
+  }
+
