73e4e051ef5800c95bf7533f11aead0545a42d55
AnnaWijetunga <annawijetunga@gmail.com>
2020-07-31 16:33:25 -0400

    Prevent from opening a new browser window

--- scripts.js
+++ scripts.js
@@ -43,19 +43,18 @@ function timer(seconds) {
     endTime.textContent = `Be Back At ${adjustedHour}:${minutes < 10 ? '0' : ''}${minutes}`;
   }
 
-
   function startTimer() {
     const seconds = parseInt(this.dataset.time);
     timer(seconds);
   }
 
-
   buttons.forEach(button => button.addEventListener('click', startTimer));
   document.customForm.addEventListener('submit', function(e) {
-  e.preventDefault();
-  const mins = this.minutes.value;
-  console.log(mins);
-  timer(mins * 60);
-  this.reset();
-});
+    // prevent from opening a new browser window
+    e.preventDefault();
+    const mins = this.minutes.value;
+    console.log(mins);
+    timer(mins * 60);
+    this.reset();
+  });
 
